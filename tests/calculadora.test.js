b81d485ca029dcd607146d2f6286850387d82910
Ballao.Dev <mateusrduarte@gmail.com>
2026-05-26 15:30:59 -0300

    adiciona endpoint `GET /api/v1/status` com teste unitário

--- tests/calculadora.test.js
+++
@@ -1,6 +0,0 @@
-const calculadora = require("../models/calculadora");
-
-test("A soma de 2 + 2 tem que ser 4", () => {
-  const resultada = calculadora.somar(2, 2);
-  expect(resultada).toBe(4);
-});
