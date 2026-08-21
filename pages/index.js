b02e8762f9ea326844c8f9c68f8aa00756f0e68a
Mateus Ballao <mateusrduarte@gmail.com>
2026-08-21 13:20:06 +0000

    atualiza a página inicial para incluir informações sobre a experiência do usuário

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,18 @@
 function Home() {
-  return <h1>Página em criação.</h1>;
+  return (
+    <>
+      <h1>Página em criação.</h1>
+      <br />
+      <p>Estamos trabalhando para trazer a melhor experiência possível.</p>
+      <br />
+      <a href="/rubeni/capacidade_de_carga.html">
+        <button type="button">capacidade_de_carga.html</button>
+      </a>
+      <a href="/rubeni/pedagio_nova364.html" style={{ marginLeft: "8px" }}>
+        <button type="button">pedagio_nova364.html</button>
+      </a>
+    </>
+  );
 }
 
 export default Home;
